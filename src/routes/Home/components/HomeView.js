8dc8c0f0e4dee899b2e6324d05e080bd2cd3a5ab
pecora <pecora9264@163.com>
2017-08-14 10:41:09 +0800

    17-08-14

---
+++ src/routes/Home/components/HomeView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from './Carousel.jsx'
+import './HomeView.scss'
+
+export const HomeView = () => (
+  <div className='bb'>
+    <h4>2017,我毕业了!</h4>
+    <Carousel/>
+  </div>
+)
+
+export default HomeView
